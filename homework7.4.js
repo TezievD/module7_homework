2f3a6294aa6e61d54085876dc5cbfc2d3f70f48a
TezievD <110922411+TezievD@users.noreply.github.com>
2023-02-17 20:11:27 +0300

    Create homework7.4.js

---
+++ homework7.4.js
@@ -0,0 +1,40 @@
+//Задание 1.
+
+const user = {
+	имя: 'Dzantemir',
+	фамилия: 'Teziev'
+}; 
+
+function db(usr) {
+	for (let key in usr) {
+		
+		 if (usr.hasOwnProperty(key)) {
+
+			console.log(`${key}: ${usr[key]}`); 
+		};   
+	 
+	};
+	
+};
+db(user);
+
+//Задание 2.
+
+ function check(str, obj) {
+	
+	console.log(str in obj);
+	
+};
+check('фамилия', user);
+
+//Задание 3.
+
+function createObj(obj) {
+	let staples = {};
+	
+	return obj = staples;
+  }
+
+  let object = createObj('obj')
+  console.log((object));
+ 
